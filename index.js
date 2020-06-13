9dfc21171664ac99248d56789971bfacc7d03119
HelaGone <luah613@gmail.com>
2020-06-13 15:37:07 -0500

    add events for tracking pwa

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
 document.addEventListener('DOMContentLoaded', event => {
+  let displayMode = 'browser tab';
+  let deferredPrompt;
   if('serviceWorker' in navigator){
     navigator.serviceWorker.register('./sw.js').then(registration => {
     }, error => {
       console.error(error);
     })
   }
+
+  window.addEventListener('beforeinstallprompt', (e)=>{
+    e.preventDefault();
+    deferredPrompt = e;
+    // console.log("event: ", e);
+  });
+
+  window.addEventListener('appinstalled', (e) =>{
+    // console.log("[INSTALL]: Success", );
+    gtag('event', 'app_install', {
+			'event_category': 'pwa',
+			'event_label': 'install',
+			'value': 'true'
+		});
+  });
+
+  if(navigator.standalone){
+    displayMode = 'standalone-ios';
+  }
+  if(window.matchMedia('(display-mode: standalone)').matches){
+    displayMode = 'standalone';
+  }
+
+  window.matchMedia('(display-mode: standalone)').addListener((e) => {
+    if(e.matches){
+      displayMode = 'standalone';
+    }
+  })
+
+  // console.log("[DISPLAY_MODE]: ", displayMode);
+
+  gtag('event', 'display_mode', {
+    'event_category': 'pwa',
+    'event_label': 'display',
+    'value': displayMode
+  });
+
+  let btnDownload = document.getElementById('btn_download');
+  btnDownload.addEventListener('click', (e) => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choice) => {
+      if(choice.outcome === 'accepted'){
+        gtag('event', 'accepted', {
+          'event_category': 'pwa',
+          'event_label': 'accept',
+          'value': 'true'
+        });
+      }else{
+        gtag('event', 'dismissed', {
+          'event_category': 'pwa',
+          'event_label': 'dismiss',
+          'value': 'true'
+        });
+      }
+    })
+  });
+
 });
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,53 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
-  workbox.precaching.precacheAndRoute([]);
 
-  workbox.routing.registerRoute(
+	workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);//End precache
+
+	workbox.googleAnalytics.initialize({
+		parameterOverrides: {
+			cd1: "Offline"
+		}
+	});
+
+	//Ignore preview and admin files
+	workbox.routing.registerRoute(
+	  /wp-admin(.*)|(.*)preview=true(.*)|(.*)\/plugins\/(.*)/,
+	  new workbox.strategies.NetworkOnly()
+	);
+
+	//Stale while revalidate for js and css that are not in precache
+	workbox.routing.registerRoute(
+	  /\.(?:js|css)$/,
+	  new workbox.strategies.StaleWhileRevalidate()
+	);
+
+	//Limit to 50 images in cache
+	workbox.routing.registerRoute(
+	  /\.(?:png|jpg|svg|gif|webp)(.*)$/,
+	  new workbox.strategies.CacheFirst({
+	    cacheName: 'images-cache',
+	    cacheExpiration: {
+	      maxEntries: 50
+	    }
+	  })
+	);
+
+	//Cache wp pages
+	workbox.routing.registerRoute(
 	  new RegExp('/(.*)/'),
 	  new workbox.strategies.NetworkFirst({
 	    cacheName: 'pages-cache',
 	    plugins: [
-				new workbox.expiration.Plugin({
-					maxEntries: 5,
+				new workbox.expiration.ExpirationPlugin({
+					maxEntries: 20,
 					maxAgeSeconds: 60 * 60 * 24
 				}),
-	      new workbox.cacheableResponse.Plugin({
+	      new workbox.cacheableResponse.CacheableResponsePlugin({
 	        statuses: [0, 200]
 	      })
 	    ]
 	  })
 	);
+
 }
